6b3305daf99d6804a8fddad664a9f0664fff7ef3
Rauloido <94703826+Rauloido@users.noreply.github.com>
2025-12-27 18:17:03 +0200

    ft

--- sw.js
+++ sw.js
@@ -1,19 +1,5 @@
 const CACHE_NAME = 'countdown-cache-v1';
 const ASSETS = [
-  './index.html',
-  './manifest.json',
-  './icons/icon.png'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(caches.match(event.request).then((resp) => resp || fetch(event.request)));
-});
-const CACHE_NAME = 'countdown-cache-v1';
-const ASSETS = [
   './Untitled-1.html',
   './manifest.json',
   './icons/icon.svg'
