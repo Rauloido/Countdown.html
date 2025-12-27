abc2557bc4c9fdf455f5351ea63fa68dcd7684b1
Rauloido <94703826+Rauloido@users.noreply.github.com>
2025-12-27 17:36:16 +0200

    icons aopple

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'countdown-cache-v1';
+const ASSETS = [
+  './Untitled-1.html',
+  './manifest.json',
+  './icons/icon.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => resp || fetch(event.request))
+  );
+});
